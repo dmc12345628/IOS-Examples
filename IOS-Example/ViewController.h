c2c730adc6c7d55702a7766b2cdcbb10d9d3f80f
adminsige <local192@eduroam10073.univ-lr.fr>
2018-01-16 09:13:14 +0100

    Initial Commit

---
+++ IOS-Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS-Example
+//
+//  Created by local192 on 16/01/2018.
+//  Copyright © 2018 local192. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOS-Example/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IOS-Example
+//
+//  Created by local192 on 16/01/2018.
+//  Copyright © 2018 local192. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
